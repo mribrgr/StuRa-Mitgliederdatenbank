55f7400c4635968e3bb5d92af2dc6ed160dc27e1
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-03-20 11:45:01 +0100

    Register apps

--- bin/settings.py
+++ bin/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aemter',
+    'historie',
+    'login',
+    'mitglieder',
+    'nutzer'
 ]
 
 MIDDLEWARE = [
