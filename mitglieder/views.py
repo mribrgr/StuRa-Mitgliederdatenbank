8a0862f5399c647c0c4bbb5ce3cb113c9c5aeb6c
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-03-25 10:53:44 +0100

    create main view

--- mitglieder/views.py
+++ mitglieder/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main_screen(request):
-    return HttpResponse("This is the mitglieder screen")
+
+    numbers = [1, 2, 3, 4, 5]
+
+    return render(request=request,
+                  template_name="mitglieder/mitglieder.html",
+                  context = {"data":numbers})
+
+#
+# TODO: Richtige Probe Datensätze
+# TODO: Preview Fesnster mit mehr inhalten
+# 
