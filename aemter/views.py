c911cedfb696732d736620411aae5721c2b217cd
Theresa <s79136@informatik.htw-dresden.de>
2020-03-27 19:59:17 +0100

    added missing file

--- aemter/views.py
+++ aemter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-import random
+from django.http import HttpResponse
 
 
 # Create your views here.
--- checklisten/views.py
+++ checklisten/views.py
@@ -4,4 +4,54 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main_screen(request):
-    return HttpResponse("This is the checklisten screen")
+    context = {
+        'mitglieder': [{
+                'name': 'Angela Merkel',
+                'aufgaben': [
+                    {'name': 'Mailverteiler'},
+                    {'name': 'Schließberechtigung'},
+                    {'name': 'Berechtigung Website'}
+                ]
+            },
+            {
+                'name': 'Jesus Christus',
+                'aufgaben': [
+                    {'name': 'Schließberechtigung'},
+                    {'name': 'Berechtigung Website'}
+                ]
+            },
+            {
+                'name': 'Steve Harris',
+                'aufgaben': [
+                    {'name': 'Mailverteiler'},
+                    {'name': 'Berechtigung Website'}
+                ]
+            },
+            {
+                'name': 'Dave Lombardo',
+                'aufgaben': [
+                    {'name': 'Mailverteiler'},
+                    {'name': 'Schließberechtigung'},
+                ]
+            },
+            {
+                'name': 'Kerry King',
+                'aufgaben': [
+                    {'name': 'Berechtigung Website'}
+                ]
+            },
+            {
+                'name': 'Otto Normalverbraucher',
+                'aufgaben': [
+                    {'name': 'Mailverteiler'}
+                ]
+            },
+            {
+                'name': 'Odowakar Rakawodo',
+                'aufgaben': [
+                    {'name': 'Schließberechtigung'}
+                ]
+            }
+        ]
+    }
+    return render(request, 'checklisten/main_screen.html', context)
