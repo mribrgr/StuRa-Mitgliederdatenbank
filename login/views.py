4af94dc2c23b17063ddec1a623432b6d6cbeacfd
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-07 18:20:24 +0200

    Add login functionality

--- login/views.py
+++ login/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+from django import forms
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def main_screen(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"Herzlich Willkommen, {username}!")
+                return redirect('/mitglieder')
+            else:
+                messages.error(request, "Benutzername oder Passwort ungültig.")
+        else:
+            messages.error(request, "Benutzername oder Passwort ungültig.")
+
+    form = AuthenticationForm()
+
     return render(request=request,
-                  template_name="login/login.html")
+                  template_name="login/login.html",
+                  context={"form":form})
