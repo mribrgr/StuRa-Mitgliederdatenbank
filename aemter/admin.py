08ae751d28b6895b3e89cdfc1f35d1471c70e1b4
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-19 17:10:38 +0200

    Register models with history

--- aemter/admin.py
+++ aemter/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
-from mitglieder.models import Mitglied
-from aemter.models import Amt
+from mitglieder.models import Mitglied, MitgliedMail, MitgliedAmt
+from aemter.models import Referat, Unterbereich, Amt, Recht, AmtRecht
 
-admin.site.register(Mitglied)
-admin.site.register(Amt)
+admin.site.register(Mitglied, SimpleHistoryAdmin)
+admin.site.register(MitgliedMail, SimpleHistoryAdmin)
+admin.site.register(MitgliedAmt, SimpleHistoryAdmin)
+
+admin.site.register(Referat, SimpleHistoryAdmin)
+admin.site.register(Unterbereich, SimpleHistoryAdmin)
+admin.site.register(Amt, SimpleHistoryAdmin)
+admin.site.register(Recht, SimpleHistoryAdmin)
+admin.site.register(AmtRecht, SimpleHistoryAdmin)
