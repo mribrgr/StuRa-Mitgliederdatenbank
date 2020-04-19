42822eabd56c6b50df02b08c28210c3aaa5cc7e4
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-19 17:02:45 +0200

    Add history to models

--- mitglieder/models.py
+++ mitglieder/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 from aemter.models import Amt
 
@@ -11,17 +12,20 @@ class Mitglied(models.Model):
     plz = models.CharField(max_length=5, null=True)
     ort = models.CharField(max_length=50, null=True)
     tel_mobil = models.CharField(max_length=15, null=True)
+    history = HistoricalRecords()
     def __str__(self):
         return self.vorname + " " + self.name
 
 class MitgliedAmt(models.Model):
     mitglied = models.ForeignKey(Mitglied, on_delete=models.CASCADE, null=False)
     amt = models.ForeignKey(Amt, on_delete=models.CASCADE, null=False)
+    history = HistoricalRecords()
     def __str__(self):
         return self.mitglied.__str__() + ", " + self.amt.__str__()
 
 class MitgliedMail(models.Model):
     mitglied = models.ForeignKey(Mitglied, on_delete=models.CASCADE, null=False)
     email = models.CharField(max_length=50, null=False)
+    history = HistoricalRecords()
     def __str__(self):
         return self.email + " " + self.mitglied.__str__()
