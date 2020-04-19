39ea3ebeb71d33cab9aade2e85936d17ca1834b7
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-19 16:56:58 +0200

    Add django-simple-history and middleware

--- bin/settings.py
+++ bin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simple_history',
     'bin',
     'aemter',
     'login',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'bin.urls'
