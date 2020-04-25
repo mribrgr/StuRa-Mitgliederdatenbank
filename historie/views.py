d8f3c0ecfbafe821490f9f6b1ee5335dff7f5c83
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-25 12:01:42 +0200

    Add proof-of-concept for Mitglieder Historie

--- historie/views.py
+++ historie/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.template import RequestContext
 
+from datetime import datetime
+
+from mitglieder.models import Mitglied, MitgliedMail, MitgliedAmt
+from aemter.models import Referat, Unterbereich, Amt, Recht, AmtRecht
 
 # Create your views here.
 def list(request):
+    # Access restrictions
+    if not request.user.is_authenticated:
+        messages.error(request, "Du musst angemeldet sein, um diese Seite sehen zu können.")
+        return redirect("/")
+    if not request.user.is_superuser:
+        messages.error(request, "Du musst Admin sein, um diese Seite aufrufen zu können.")
+        return redirect("/mitglieder")
+
+    # Fetch data
+    mitglied = Mitglied.history.all()
+
     return render(request=request,
-                  template_name="historie/list.html")
+                  template_name="historie/list.html",
+                  context={"mitglied":mitglied})
