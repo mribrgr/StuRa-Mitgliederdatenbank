d82ba0b9780429776c027c83d71e93354021ae36
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-04-25 13:59:48 +0200

    refactor

--- tests/test_004_mitgliedHinzufuegen.py
+++ tests/test_004_mitgliedHinzufuegen.py
@@ -1,105 +1,30 @@
-from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.urls import reverse
 import time
-from django.contrib.auth import get_user_model
-from aemter.models import Amt, Unterbereich, Referat
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from django.urls import reverse
 
-import csv
+from tests.MyTestCase import MyTestCase, loginAsLukasAdmin
 
-class TestMitgliedHinzufuegen(StaticLiveServerTestCase):
-
-    # befor every test funktion
-    def setUp(self):
-        """
-            hier muss der richtige Webtreiber ausgewählt werden
-            1. Edge - Windows
-            2. Firefox - Windows
-            3. Firefox - Linux 
-        """
-        #self.browser = webdriver.Edge('tests\\edgedriver_win64\\msedgedriver.exe')
-        self.browser = webdriver.Firefox(executable_path='tests\\firefoxdriver-win64\\geckodriver.exe')
-        #self.browser = webdriver.Firefox(executable_path='tests\\firefoxdriver-linux64\\geckodriver')
-        pass
-
-    # after every test funktion
-    def tearDown(self):
-        try:
-            self.browser.close()
-        except:
-            print('Error while closing')
-        pass
+class TestMitgliedHinzufuegen(MyTestCase):
+    """
+        Setup and Teardown Funktions are specified in
+        MyTestCase
+    """
 
     # Tests
     def test_1MitgliedHinzufügen_AsSuperuser(self):
-        user = get_user_model().objects.create_superuser(username='testlukas', password='0123456789test')
-
         try:
             self.browser.get(self.live_server_url)
         except:
             print('Error in opening login page')
 
-        # Suche aller Objekte der Seite
-        try:
-            entUsername = self.browser.find_element_by_id('id_username')
-            entPassword = self.browser.find_element_by_id('id_password')
-            btnLogin = self.browser.find_element_by_id('btn-login')
-        except:
-            print("Es wurden nicht alle Objekte auf der Seite gefunden")
-
-        # Eingabe der Login-Daten
-        entUsername.send_keys('testlukas')
-        entPassword.send_keys('0123456789test')
-        btnLogin.click()
-
-        # Check Login Success
-        self.assertEquals(self.browser.current_url, self.live_server_url + reverse('mitglieder:homepage'), 
-            msg="Konnte nicht angemeldet werden bzw. Weiterleitung nicht erfolgt")
-
         """
-            Kopiert von Theresa
+            Anmelden als Admin
         """
-        Referat.objects.all().delete()
-        Unterbereich.objects.all().delete()
-        Amt.objects.all().delete()
-
-        with open("referate.csv") as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            for row in reader:
-                for referat in row:
-                    if not Referat.objects.filter(bezeichnung=referat).exists():
-                        r = Referat(bezeichnung=referat)
-                        r.save()
-                        print("Referat " + referat + " gespeichert")
-                        # Aemter
-                        a = Amt(bezeichnung="Leitung", referat=r)
-                        a.save()
-                        a = Amt(bezeichnung="Stellvertretende Leitung", referat=r)
-                        a.save()
-
-                    else:
-                        print("Referat " + referat + " existiert bereits")
-
-        with open("bereiche.csv") as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            for row in reader:
-                referat = row.pop(0)
-                print(referat)
-                for bereich in row:
-                    if Unterbereich.objects.filter(bezeichnung=bereich).exists()==False:
-                        print("Bereich " + bereich + " (Referat: " + referat + ") wird gespeichert")
-                        b = Unterbereich(bezeichnung=bereich, referat=Referat.objects.get(bezeichnung=referat))
-                        b.save()
-                        # Aemter
-                        a = Amt(bezeichnung="Leitung", unterbereich=b, referat=b.referat)
-                        a.save()
-                        a = Amt(bezeichnung="Stellvertretende Leitung", unterbereich=b, referat=b.referat)
-                        a.save()
-                        a = Amt(bezeichnung="Beratendes Mitglied", unterbereich=b, referat=b.referat)
-                        a.save()
-        """
-            Kopiert von Theresa
-        """
+        loginAsLukasAdmin(self)
 
         # Suchen des Hinzufügen Buttons
         try:
@@ -135,9 +60,15 @@ class TestMitgliedHinzufuegen(StaticLiveServerTestCase):
         entSpitzname.send_keys('Hansi')
 
         """
-            TODO: Amt Hinzufügen
             TODO: Amt bzw. Referat auswählen
         """
+        time.sleep(120)
+        referat = Select(self.browser.find_element_by_id('selectreferat1'))
+        self.browser.find_element_by_xpath("//select[@name='selectreferat1']/option[text()='Finanzen']").click()
+        #print(referat)
+        #WebDriverWait(self.browser, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, "//select[@id='selectreferat1']//options[contains(.,'Finanzen')]")))
+        #referat.select_by_visible_text('Finanzen')
+        
 
         btnAddEmail.click()
         # Finden der Email Felder
@@ -158,8 +89,7 @@ class TestMitgliedHinzufuegen(StaticLiveServerTestCase):
         entOrt.send_keys('Ebersbach')
         entTelefon_mobil.send_keys('0362594833')
 
-        time.sleep(50)
-
         # Speichern der Daten
         #btnSave.click()
+        time.sleep(120)
         pass
