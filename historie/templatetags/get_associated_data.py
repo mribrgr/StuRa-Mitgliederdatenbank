74e7ad0286b0676b9100f490ac45bf818f095fb4
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-04-27 18:18:57 +0200

    Begin adding associated data to history entries

---
+++ historie/templatetags/get_associated_data.py
@@ -0,0 +1,36 @@
+from django import template
+
+from mitglieder.models import Mitglied, MitgliedMail, MitgliedAmt
+from aemter.models import Referat, Unterbereich, Amt, Recht, AmtRecht
+
+register = template.Library()
+
+@register.simple_tag
+def get_associated_data(desiredInfo, queryType, primaryKey, timestamp):
+    if(desiredInfo == "Mitglied"): foreignClass = Mitglied
+    if(desiredInfo == "Amt"): foreignClass = Amt
+    if(desiredInfo == "Unterbereich"): foreignClass = Unterbereich
+    if(desiredInfo == "Referat"): foreignClass = Referat
+
+    if primaryKey == "": return None
+
+    if queryType == "latest":
+        # If the referenced object is still in the database
+        associatedEntry = foreignClass.objects.filter(id=primaryKey).first()
+        if associatedEntry == None:
+            # If the referenced object has been deleted
+            associatedEntry = foreignClass.history.filter(id=primaryKey).order_by("-history_date").first()
+
+    if queryType == "historical":
+        # If the referenced object is still in the database
+        instance = foreignClass.objects.filter(id=primaryKey).first()
+        if instance != None:
+            try:
+                associatedEntry = instance.history.as_of(timestamp)
+            except:
+                associatedEntry = None
+        else:
+            # If the referenced object has been deleted
+            associatedEntry = foreignClass.history.filter(id=primaryKey).filter(history_date__lte=timestamp).order_by("-history_date").first()
+
+    return associatedEntry
