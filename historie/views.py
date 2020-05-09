1825b2c525b3bea064912f458a820e5f7fc5face
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-05-09 21:58:01 +0200

    Add clear button and no-results-found message

--- historie/views.py
+++ historie/views.py
@@ -36,21 +36,29 @@ def list(request):
         aemterRechte = AmtRecht.history.none()
         users = User.history.none()
 
+        # Iterate over all given search terms and fetch all entries matching any of the terms
         for term in searchterms:
-            # Fetch all entries matching the search term
+            term = term.strip()
             mitglieder = mitglieder | Mitglied.history.filter(Q(id__icontains=term) | Q(vorname__icontains=term) | Q(name__icontains=term))
             mitgliederMails =  mitgliederMails | MitgliedMail.history.filter(Q(mitglied__id__icontains=term) | Q(mitglied__vorname__icontains=term) | Q(mitglied__name__icontains=term) | Q(email__icontains=term))
-            mitgliederAemter = mitgliederAemter | MitgliedAmt.history.filter(Q(mitglied__id__icontains=term) | Q(mitglied__vorname__icontains=term) | Q(mitglied__name__icontains=term) | Q(amt__id__icontains=term) | Q(amt__bezeichnung__icontains=term) | Q(amt__referat__bezeichnung__icontains=term) | Q(amt__unterbereich__bezeichnung__icontains=term))
+            mitgliederAemter = mitgliederAemter | MitgliedAmt.history.filter(Q(mitglied__id__icontains=term) | Q(mitglied__vorname__icontains=term) | Q(mitglied__name__icontains=term) 
+                | Q(amt__id__icontains=term) | Q(amt__bezeichnung__icontains=term) 
+                | Q(amt__referat__bezeichnung__icontains=term)
+                | Q(amt__unterbereich__bezeichnung__icontains=term))
 
             referate = referate | Referat.history.filter(Q(id__icontains=term) | Q(bezeichnung__icontains=term))
             unterbereiche = unterbereiche | Unterbereich.history.filter(Q(id__icontains=term) | Q(bezeichnung__icontains=term) | Q(referat__id__icontains=term) | Q(referat__bezeichnung__icontains=term))
-            aemter = aemter | Amt.history.filter(Q(id__icontains=term) | Q(bezeichnung__icontains=term) | Q(referat__id__icontains=term) | Q(referat__bezeichnung__icontains=term) | Q(unterbereich__id__icontains=term) | Q(unterbereich__bezeichnung__icontains=term))
+            aemter = aemter | Amt.history.filter(Q(id__icontains=term) | Q(bezeichnung__icontains=term) 
+                | Q(referat__id__icontains=term) | Q(referat__bezeichnung__icontains=term) 
+                | Q(unterbereich__id__icontains=term) | Q(unterbereich__bezeichnung__icontains=term))
             rechte = rechte | Recht.history.filter(Q(id__icontains=term) | Q(bezeichnung__icontains=term))
-            aemterRechte = aemterRechte | AmtRecht.history.filter(Q(amt__id__icontains=term), Q(amt__bezeichnung__icontains=term) | Q(recht__id__icontains=term) |Q(recht__bezeichnung__icontains=term))
+            aemterRechte = aemterRechte | AmtRecht.history.filter(Q(amt__id__icontains=term) | Q(amt__bezeichnung__icontains=term) 
+                | Q(amt__referat__bezeichnung__icontains=term)
+                | Q(amt__unterbereich__bezeichnung__icontains=term)
+                | Q(recht__id__icontains=term) |Q(recht__bezeichnung__icontains=term))
 
             users = users | User.history.filter(Q(username__icontains=term) | Q(first_name__icontains=term) | Q(last_name__icontains=term) | Q(email__icontains=term))
     else:
-        searchterm=""
         # Fetch all entries
         mitglieder = Mitglied.history.all()
         mitgliederMails = MitgliedMail.history.all()
@@ -63,6 +71,8 @@ def list(request):
         aemterRechte = AmtRecht.history.all()
 
         users = User.history.all()
+
+        searchterm=""
 
     return render(request=request,
                   template_name="historie/list.html",
