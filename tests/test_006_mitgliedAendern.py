71610040698ef75d8a4f026a983730a51e72a6f0
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-05-17 11:15:27 +0200

    Auskommentieren der vorher fehlerhaften funktionen

--- tests/test_006_mitgliedAendern.py
+++ tests/test_006_mitgliedAendern.py
@@ -8,7 +8,7 @@ class TestMitgliedAendern(MyTestCase):
         Setup and Teardown Funktions are specified in
         MyTestCase
     """
-    
+
     # Tests
     def test_1MitgliedAendern_AsSuperuser(self):
         time.sleep(5)
@@ -16,26 +16,28 @@ class TestMitgliedAendern(MyTestCase):
             self.browser.get(self.live_server_url)
         except:
             print('Error in opening login page')
-        
-        
+
+
         time.sleep(5)
         """
             Login as Admin + Add an Mitglied
         """
         loginAsLukasAdmin(self)
-        addMitglied(self) 
+        addMitglied(self)
 
         """
             Auswählen eines Mitglieds (Stift)
         """
-        #self.browser.find_element_by_xpath("//tr[@id='tr1']/td/a").click()
+        self.browser.find_element_by_xpath("//tr[@id='tr1']/td/a").click()
 
         """
             Verändern von Feldern
         """
-        #self.browser.find_element_by_xpath("//input[@id='vorname']").send_keys('_1')
-        #self.browser.find_element_by_xpath("//input[@id='nachname']").send_keys('_1')
-        #self.browser.find_element_by_xpath("//a[@id='save_button']").click()
+        self.browser.find_element_by_xpath("//input[@id='vorname']").send_keys('_1')
+        self.browser.find_element_by_xpath("//input[@id='nachname']").send_keys('_1')
+        self.browser.find_element_by_xpath("//a[@id='save_button']").click()
 
-        time.sleep(120)
+        self.assertEqual(self.browser.find_element_by_xpath("//tr[@class='mitglied']/td[contains(text(), 'Hans_1 Peter_1')]").text,
+                            "Hans_1 Peter_1",
+                            msg="Hans Peter wurde nicht geändert")
         pass
