1b11f13e2af77d7b63698fb03935c56e00fdccac
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-05-25 08:31:46 +0200

    finish test_007

--- tests/test_007_aemtHinzufuegen.py
+++ tests/test_007_aemtHinzufuegen.py
@@ -3,11 +3,17 @@ import time
 from tests.MyTestCase import MyTestCase, loginAsLukasAdmin
 from tests.MyFuncAemter import createAmt, createReferat, createUnterbereich
 from selenium.webdriver.support.ui import Select
+from django.urls import reverse
 
 class TestAemtHinzufuegen(MyTestCase):
     """
         Hier wird getestet:
+
+        * Ob ein Referat richtig über das AdminPanel Hinzugefügt wird
+        * Ob ein Unterbereich richtig über das AdminPanel Hinzugefügt wird
         * Ob ein Amt richtig über das AdminPanel Hinzugefügt wird
+        * Ein Komplextest ob man mit Hinzugefügten Referat/Unterbereich/Amt
+        Ein neues Mitglied erstellen kann, und ob es in der Ämterübersicht übernommen wird
     """
 
     def test_1ReferatHinzufuegen_AsSuperuser(self):
@@ -72,6 +78,9 @@ class TestAemtHinzufuegen(MyTestCase):
     def test_ReferatUnterbereichAmtHinzufuegen_AsSuperuser(self):
         """
             Dies ist ein Zusammenhängender Test
+            Referat, Unterbereich und Amt werden neu angelegt.
+            Ein neues Mitglied wird erstellt und diesem zugeordnet.
+            Es wird Überprüft ob auch alles in die Ämterübersicht mitgenommen wird.
         """
         try:
             self.browser.get(self.live_server_url)
@@ -108,15 +117,56 @@ class TestAemtHinzufuegen(MyTestCase):
         self.browser.find_element_by_xpath("//div[@id='div_selectreferat1']/div/input").click()
         self.browser.find_element_by_xpath("//span[text()='%s']"%referat).click()
 
-        """
-            TODO:
-            Hier weiter machen
-        """
+        self.browser.find_element_by_xpath("//div[@id='div_selectbereich1']/div/div/input").click()
+        self.browser.find_element_by_xpath("//span[text()='%s']"%unterbereich).click()
+
+        self.browser.find_element_by_xpath("//div[@id='div_selectamt1']/div/div/input").click()
+        self.browser.find_element_by_xpath("//span[text()='%s']"%amt).click()
+
+        # weitere Daten Hinzufügen
+        self.browser.find_element_by_name('vorname').send_keys('Hans')
+        self.browser.find_element_by_name('nachname').send_keys('Peter')
+        self.browser.find_element_by_name('spitzname').send_keys('Hansi')
+        self.browser.find_element_by_name('email1').send_keys('sxxxxx@htw-dresden.de')
+        self.browser.find_element_by_name('strasse').send_keys('Straße der Freiheit')
+        self.browser.find_element_by_name('hausnr').send_keys('24')
+        self.browser.find_element_by_name('plz').send_keys('01561')
+        self.browser.find_element_by_name('ort').send_keys('Ebersbach')
+        self.browser.find_element_by_name('telefon_mobil').send_keys('0362594833')
+
+        # Speichern
+        self.browser.find_element_by_id('save_button').click()
+        self.assertEqual(self.browser.current_url,
+                            self.live_server_url + reverse('mitglieder:homepage'),
+                            msg="Weiterleitung nicht erfolgt")
+        self.assertEqual(self.browser.find_element_by_xpath("//tr[@class='mitglied']/td[contains(text(), 'Hans Peter')]").text,
+                            "Hans Peter",
+                            msg="Hans Peter wurde nicht angelegt")
+        searchstring = amt + " " + unterbereich + " (Referat " + referat + ")"
+        self.assertEqual(self.browser.find_element_by_xpath("//tr[@class='mitglied']/td/ul/li[contains(text(), '%s')]"%searchstring).text,
+                            searchstring,
+                            msg="Amt wurde nicht richtig zugewiesen")
 
         """
-            TODO:
             Schauen in der Ämter Übersicht ob alles angezeigt wird
         """
+        # navigieren zur Ämterübersicht
+        self.browser.find_element_by_xpath("//a[@href='/aemter']").click()
 
-        time.sleep(200)
+        # öffnen der collabseables
+        searchstring = "Referat " + referat
+        self.browser.find_element_by_xpath("//div[text()='%s']"%searchstring).click()
+        searchstring = "Bereich " + unterbereich
+        self.browser.find_element_by_xpath("//div[text()='%s']"%searchstring).click()
+
+        # überprüfen ob Amt da ist
+        self.assertEqual(self.browser.find_element_by_xpath("//tr/td[contains(text(), '%s')]"%amt).text,
+                            amt,
+                            msg="Amt ist nicht in Übersicht Ämter vorhanden")
+        """
+        TODO: Schauen ob Person richtig einem Amt zugeordnet wurde
+        self.assertEqual(self.browser.find_element_by_xpath("//tr/td[contains(text(), 'Hans Peter\n')]").text,
+                            "Hans Peter",
+                            msg="Hans Peter wurde nicht richtig dem Amt hinzugefügt")
+        """
         pass
