7083f659620fb5c8a0f3d50cabee83fdc74153b8
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-06-09 10:25:48 +0200

    refactoring

---
+++ importscripts/import.py
@@ -0,0 +1,62 @@
+import os, django, sys
+sys.path.append('..')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bin.settings")
+django.setup()
+
+# your imports, e.g. Django models
+from aemter.models import Referat, Amt, Unterbereich
+import csv
+
+
+def importAemter(file):
+    # Delete existing Data
+    Referat.objects.all().delete()
+    Unterbereich.objects.all().delete()
+    Amt.objects.all().delete()
+
+    # read CSV
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        referat = row[0]
+        unterbereich = row[1]
+        amt = row[2]
+
+        # Print Current Line for Debug
+        print(referat + " | " + unterbereich + " | " + amt)
+
+        if (referat == 'Referat'):
+            continue
+
+        # Erstelle das Referat
+        if not Referat.objects.filter(bezeichnung=referat).exists():
+            new_referat = Referat(
+                bezeichnung = referat
+            )
+            new_referat.save()
+        else:
+            new_referat = Referat.objects.get(bezeichnung=referat)
+
+        # Erstelle den Unterbereich
+        if not Unterbereich.objects.filter(bezeichnung=unterbereich).exists():
+            new_unterbereich = None
+            if (unterbereich != 'None'):
+                new_unterbereich = Unterbereich(
+                    bezeichnung = unterbereich,
+                    referat = new_referat
+                )
+                new_unterbereich.save()
+        else:
+            new_unterbereich = Unterbereich.objects.get(bezeichnung=unterbereich)
+
+        # Erstelle das Amt
+        new_amt = Amt(
+            bezeichnung = amt,
+            workload = 5,
+            referat = new_referat,
+            unterbereich = new_unterbereich
+        )
+        new_amt.save()
+    pass
+
+file = open("ReferateUnterbereicheAemter.csv")
+importAemter(file)
