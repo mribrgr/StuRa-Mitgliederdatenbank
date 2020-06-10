11ee2c7abf8b7f011388ffc946cbc16a0ddea986
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-06-10 18:50:01 +0200

    try to run test on raspberry

--- tests/MyTestCase.py
+++ tests/MyTestCase.py
@@ -36,13 +36,12 @@ class MyTestCase(StaticLiveServerTestCase):
                     keep_alive=True)
                 pass
             if system() == 'Linux':
-                self.browser = WebDriver(
-                    timeout=5,
-                    firefox_options=options,
-                    log_path='django.log',
-                    keep_alive=True
-                )
+                self.browser = WebDriver()
+                # timeout=5,
                 # executable_path='tests/firefoxdriver-linux64/geckodriver',
+                # firefox_options=options,
+                # log_path='django.log',
+                # keep_alive=True
                 pass
 
             self.browser.implicitly_wait(5)
