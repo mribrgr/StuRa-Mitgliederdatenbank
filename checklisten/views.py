0707d1ac73fe4a806f6a25194fe8470d93a9ab48
Benjamin Hempel <benjamin.hempel5@gmail.com>
2020-06-22 10:18:30 +0200

    Checklisten: Add view prototypes

--- checklisten/views.py
+++ checklisten/views.py
@@ -1,60 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-# Create your views here.
+from .models import Checkliste
+
 def main_screen(request):
     if not request.user.is_authenticated:
         messages.error(request, "Du musst angemeldet sein, um diese Seite sehen zu können.")
         return redirect("/")
 
-    context = {
-        'mitglieder': [{
-                'name': 'Angela Merkel',
-                'aufgaben': [
-                    {'name': 'Mailverteiler'},
-                    {'name': 'Schließberechtigung'},
-                    {'name': 'Berechtigung Website'}
-                ]
-            },
-            {
-                'name': 'Jesus Christus',
-                'aufgaben': [
-                    {'name': 'Schließberechtigung'},
-                    {'name': 'Berechtigung Website'}
-                ]
-            },
-            {
-                'name': 'Steve Harris',
-                'aufgaben': [
-                    {'name': 'Mailverteiler'},
-                    {'name': 'Berechtigung Website'}
-                ]
-            },
-            {
-                'name': 'Dave Lombardo',
-                'aufgaben': [
-                    {'name': 'Mailverteiler'},
-                    {'name': 'Schließberechtigung'},
-                ]
-            },
-            {
-                'name': 'Kerry King',
-                'aufgaben': [
-                    {'name': 'Berechtigung Website'}
-                ]
-            },
-            {
-                'name': 'Otto Normalverbraucher',
-                'aufgaben': [
-                    {'name': 'Mailverteiler'}
-                ]
-            },
-            {
-                'name': 'Odowakar Rakawodo',
-                'aufgaben': [
-                    {'name': 'Schließberechtigung'}
-                ]
-            }
-        ]
-    }
-    return render(request, 'checklisten/main_screen.html', context)
+    checklisten = Checkliste.objects.all()
+
+    return render(request=request, 
+                  template_name='checklisten/main_screen.html', 
+                  context = {"checklisten": checklisten})
+
+def erstellen(request):
+    return redirect("/checklisten")
+
+def abhaken(request):
+    return redirect("/checklisten")
