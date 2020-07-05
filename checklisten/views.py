042ed03f965707626976422708bb87b7c6a06490
Benjamin Hempel <benjamin.hempel5@gmail.com>
2020-07-05 20:15:07 +0200

    Add Rechte to Checklisten

--- checklisten/views.py
+++ checklisten/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 
-from .models import Checkliste, ChecklisteAufgabe
+from .models import Checkliste, ChecklisteAufgabe, ChecklisteRecht
 from aemter.models import FunktionRecht
 
 def main_screen(request):
@@ -25,8 +25,20 @@ def abhaken(request):
     if not request.user.is_superuser:
         return HttpResponse("Permission denied")
 
+    task_type = request.POST.get('task_type')
+    if(task_type != "Aufgabe" and task_type != "Recht"):
+        return HttpResponse("Invalid task_type")
+
     task_id = request.POST.get('task_id')
-    task = ChecklisteAufgabe.objects.get(id=task_id)
+
+    task = None
+    if task_type == "Aufgabe":
+        task = ChecklisteAufgabe.objects.get(id=task_id)
+    if task_type == "Recht":
+        task = ChecklisteRecht.objects.get(id=task_id)
+    if not task:
+        return HttpResponse("Invalid task_id")
+
     task.abgehakt = not task.abgehakt
     task.save()
 
