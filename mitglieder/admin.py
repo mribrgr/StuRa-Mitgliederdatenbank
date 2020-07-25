46ed439f69d8454a6e31c16626fe23b8e426f863
Yoshinonxkun <45177922+Yoshinonxkun@users.noreply.github.com>
2020-07-25 17:56:28 +0200

    Delete admin.py
    Delete Mitglieder Funktionen aus dem Adminbereich

--- mitglieder/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from simple_history.admin import SimpleHistoryAdmin
-
-from .models import Mitglied, MitgliedMail, MitgliedAmt
-
-admin.site.register(Mitglied, SimpleHistoryAdmin)
-admin.site.register(MitgliedMail, SimpleHistoryAdmin)
-admin.site.register(MitgliedAmt, SimpleHistoryAdmin)
